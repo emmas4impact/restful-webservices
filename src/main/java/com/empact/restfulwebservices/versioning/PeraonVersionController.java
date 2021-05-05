a55180f66eb5f0926da81069905c5131e405b4ff
Emmanuel Adedeji <emmans4destiny@gmail.com>
2021-05-05 13:17:25 +0300

    header versioning and produces versioning

--- src/main/java/com/empact/restfulwebservices/versioning/PeraonVersionController.java
+++ src/main/java/com/empact/restfulwebservices/versioning/PeraonVersionController.java
@@ -15,5 +15,27 @@ public class PeraonVersionController {
 	public PersonV2 personV2() {
 		return new PersonV2(new Name("Emmanuel", "Adedeji"));
 	}
+	
+	@GetMapping(value="/person/header", headers="X-API-VERSION=1")
+	public PersonV1 headerV1() {
+		return new PersonV1("Emmanuel Adedeji");
+	}
+	
+	@GetMapping(value="/person/header", headers="X-API-VERSION=2")
+	public PersonV2 headerV2() {
+		return new PersonV2(new Name("Emmanuel", "Adedeji"));
+	}
+	
+	
+	@GetMapping(value="/person/produces", produces="application/vnd.company.app-v1+json")
+	public PersonV1 produceV1() {
+		return new PersonV1("Emmanuel Adedeji");
+	}
+	
+	@GetMapping(value="/person/produces", produces="application/vnd.company.app-v2+json")
+	public PersonV2 produceV2() {
+		return new PersonV2(new Name("Emmanuel", "Adedeji"));
+	}
+
 
 }
